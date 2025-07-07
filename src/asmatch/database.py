a6d6e6c6c10a7a7bb842fbc9c62588fdeb00fd4d
Marcel Wysocki <mwysocki@google.com>
2025-07-07 18:49:48 +0800

    more test cases

--- src/asmatch/database.py
+++ src/asmatch/database.py
@@ -1,8 +1,10 @@
-
 # database.py
+import os
 from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = "sqlite:///assembly.db"
+# Default to assembly.db, but allow overriding for testing
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///assembly.db")
+
 engine = create_engine(DATABASE_URL, echo=False)
 
 def create_db_and_tables():
